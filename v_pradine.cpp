047e826d9fd9e9d8fd6f387a61e27bcb5971ad8c
Žygimantas Girdauskas <zygiss.g@gmail.com>
2023-09-28 14:08:53 +0300

    Pataisytas lentelės išvedimas

--- v_pradine.cpp
+++ v_pradine.cpp
@@ -23,17 +23,18 @@ int main()
         return 0;
     }
 
-    for (int j = 0; j < n; j++) {
-        cout << "Ar " << (j + 1) << "-ojo studento pazymius norite ivesti ranka? (t/n) ";
+    for (int j = 0; j < n; j++)
+    {
+        cout << (j + 1) << "-ojo studento pazymius ir egzamina ivesite patys (p) ar norite sugeneruoti atsitiktinai (a)? ";
         char ivesties_pasirinkimas;
         cin >> ivesties_pasirinkimas;
         // jei pasirinkimas neteisingas, ivedimas kartojamas
-        while (ivesties_pasirinkimas != 't' && ivesties_pasirinkimas != 'n')
+        while (ivesties_pasirinkimas != 'p' && ivesties_pasirinkimas != 'a' && ivesties_pasirinkimas != 'P' && ivesties_pasirinkimas != 'A') 
         {
-            cout << "Neteisinga pasirinkimo reiksme. Iveskite t arba n." << endl;
+            cout << "Neteisinga pasirinkimo reiksme. Iveskite p arba a." << endl;
             cin >> ivesties_pasirinkimas;
         }
-        if (ivesties_pasirinkimas == 't')
+        if (ivesties_pasirinkimas == 'p' || ivesties_pasirinkimas == 'P')
         {
             // ivedama ranka
             laikinas = rankinis_ivedimas();
@@ -58,22 +59,18 @@ int main()
     // surusiuojama pagal varda
     std::sort(grupe.begin(), grupe.end(), palyginimas_pagal_varda);
     // spausdinama lentele
-    printf("|%20s|%20s|%20s|\n", "Vardas", "Pavarde", "Galutinis (Med.)");
-    // spausdinama pagal pasirinkima, arba vidurkis, arba mediana:
     if (vm_pasirinkimas == 'm')
     {
-        printf("|--------------------|--------------------|--------------------|\n");
-        for (auto &a : grupe)
-            printf("|%20s|%20s|%20.2f|\n", a.vardas.c_str(), a.pavarde.c_str(), a.rez); // %20.2f - %20 simboliu plotis, 2f skaiciai po kablelio
-        // panasiai kaip su - cout << std::fixed << std::setprecision(2) << a.rez << endl;
-        printf("|--------------------|--------------------|--------------------|\n");
+        printf("|%20s|%20s|%20s|\n", "Vardas", "Pavarde", "Galutinis (Med.)");
     }
     else
     {
-        printf("|--------------------|--------------------|--------------------|\n");
-        for (auto &a : grupe)
-            printf("|%20s|%20s|%20.2f|\n", a.vardas.c_str(), a.pavarde.c_str(), a.rez); // %20.2f - %20 simboliu plotis, 2f skaiciai po kablelio
-        // panasiai kaip su - cout << std::fixed << std::setprecision(2) << a.rez << endl;
-        printf("|--------------------|--------------------|--------------------|\n");
+        printf("|%20s|%20s|%20s|\n", "Vardas", "Pavarde", "Galutinis (Vid.)");
     }
+    // spausdinama pagal pasirinkima, arba vidurkis, arba mediana:
+    printf("|--------------------|--------------------|--------------------|\n");
+    for (auto &a : grupe)
+        printf("|%20s|%20s|%20.2f|\n", a.vardas.c_str(), a.pavarde.c_str(), a.rez); // %20.2f - %20 simboliu plotis, 2f skaiciai po kablelio
+    // panasiai kaip su - cout << std::fixed << std::setprecision(2) << a.rez << endl;
+    printf("|--------------------|--------------------|--------------------|\n");
 }
