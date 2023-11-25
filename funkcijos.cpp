c5b5834340cf1ac37d66fd510cb45337e378a255
Žygimantas Girdauskas <zygiss.g@gmail.com>
2023-11-25 18:30:15 +0200

    Pridetas klases Stutendai funkcionalumas vektoriaus konteineriui. Tik laiko skaicavimo programai.

--- funkcijos.cpp
+++ funkcijos.cpp
@@ -30,6 +30,22 @@ void issaugojam_duomenis(const vector<studentas>& studentai, const string& file_
     isvesties_file.close();
 }
 
+void issaugojam_duomenis_c(const vector<Studentas>& studentai, const string& file_vard) {
+    ofstream isvesties_file(file_vard);
+
+    // antrastine eilute
+    isvesties_file << left << setw(20) << "Vardas" << setw(20) << "Pavarde"
+                   << setw(20) << "Rezultatas" << endl;
+
+    // irasomi duomenys
+    for (const auto& student : studentai) {
+        isvesties_file << left << setw(20) << student.getVardas()
+                       << setw(20) << student.getPavarde()
+                       << setw(20) << student.getRez() << endl;
+    }
+
+    isvesties_file.close();
+}
 
 void duomenu_kurimas(int n_eiluciu) {
     ofstream isvesties_file("Studentai" + to_string(n_eiluciu) + ".txt");
@@ -197,6 +213,127 @@ void laiko_skaicuokle(int n_eil, int n, int rusiavimo_pasirinkimas) {
     cout << endl;
 }
 
+void laiko_skaicuokle_c(int n_eil, int n, int rusiavimo_pasirinkimas) {
+    double bendras_laikas_ivedimo = 0.0;
+    double bendras_laikas_rusiavimo = 0.0;
+    double bendras_laikas_rusiavimo_dvi = 0.0;
+    double bendras_laikas_rusiavimo_dvi_2strategija = 0.0;
+    double bendras_laikas_rusiavimo_dvi_3strategija = 0.0;
+    double bendras_laikas_rusiavimo_dvi_4strategija = 0.0;
+    double bendras_laika_issaugojimo_galvociai = 0.0;
+    double bendras_laika_issaugojimo_nuskriaustukai = 0.0;
+
+    vector<Studentas> grupe;
+    vector<Studentas> galvociai;
+    vector<Studentas> nuskriaustukai;
+
+    char vm = 'm';
+    string filename = "Studentai" + to_string(n_eil) + ".txt";
+
+    for (int i = 0; i < n; i++) {
+        grupe.clear();
+        galvociai.clear();
+        nuskriaustukai.clear();
+
+        auto laikas_pradzia = high_resolution_clock::now();
+        ivedimas_is_file_c(grupe, vm, filename);
+        auto laikas_pabaiga = high_resolution_clock::now();
+        auto laikas_ivedimo = duration_cast<milliseconds>(laikas_pabaiga - laikas_pradzia);
+
+        bendras_laikas_ivedimo += static_cast<double>(laikas_ivedimo.count());
+
+        laikas_pradzia = high_resolution_clock::now();
+        if (rusiavimo_pasirinkimas == 1) {
+            sort(grupe.begin(), grupe.end(), palyginimas_pagal_varda_c);
+        } else if (rusiavimo_pasirinkimas == 2) {
+            sort(grupe.begin(), grupe.end(), palyginimas_pagal_pavarde_c);
+        } else {
+            sort(grupe.begin(), grupe.end(), palyginimas_pagal_rezultata_c);
+        }
+        laikas_pabaiga = high_resolution_clock::now();
+        auto laikas_rusiavimo = duration_cast<milliseconds>(laikas_pabaiga - laikas_pradzia);
+
+        bendras_laikas_rusiavimo += static_cast<double>(laikas_rusiavimo.count());
+
+        // naudojam 1 strategija
+        laikas_pradzia = high_resolution_clock::now();
+        rusiuojame_i_dvi_grupes_c(grupe, nuskriaustukai, galvociai);
+        laikas_pabaiga = high_resolution_clock::now();
+        auto laikas_rusiavimo_dvi = duration_cast<milliseconds>(laikas_pabaiga - laikas_pradzia);
+
+        bendras_laikas_rusiavimo_dvi += static_cast<double>(laikas_rusiavimo_dvi.count());
+
+        // nauodjam 2 strategija
+        laikas_pradzia = high_resolution_clock::now();
+        vector<Studentas> galvociai2 = grupe;
+        sort(galvociai2.begin(), galvociai2.end(), palyginimas_pagal_rezultata_c);
+        rusiuojame_i_dvi_grupes_2_c(galvociai2, nuskriaustukai);
+        laikas_pabaiga = high_resolution_clock::now();
+        auto laikas_rusiavimo_dvi_2strategija = duration_cast<milliseconds>(laikas_pabaiga - laikas_pradzia);
+
+        bendras_laikas_rusiavimo_dvi_2strategija += static_cast<double>(laikas_rusiavimo_dvi_2strategija.count());
+
+        // naudojam 3 strategija
+        vector<Studentas> galvociai3 = grupe;
+        laikas_pradzia = high_resolution_clock::now();
+        rusiuojame_i_dvi_grupes_3_c(galvociai3, nuskriaustukai);
+        laikas_pabaiga = high_resolution_clock::now();
+        auto laikas_rusiavimo_dvi_3strategija = duration_cast<milliseconds>(laikas_pabaiga - laikas_pradzia);
+
+        bendras_laikas_rusiavimo_dvi_3strategija += static_cast<double>(laikas_rusiavimo_dvi_3strategija.count());
+
+        // naudojam 4 strategija
+        vector<Studentas> grupe4 = grupe;
+        laikas_pradzia = high_resolution_clock::now();
+        rusiuojame_i_dvi_grupes_4_c(grupe4, nuskriaustukai, galvociai);
+        laikas_pabaiga = high_resolution_clock::now();
+        auto laikas_rusiavimo_dvi_4strategija = duration_cast<milliseconds>(laikas_pabaiga - laikas_pradzia);
+
+        bendras_laikas_rusiavimo_dvi_4strategija += static_cast<double>(laikas_rusiavimo_dvi_4strategija.count());
+
+        // issaugojam galvociu duomenis
+        laikas_pradzia = high_resolution_clock::now();
+        string galvociai_pav = "galvociai" + to_string(n_eil) + ".txt";
+        issaugojam_duomenis_c(galvociai, galvociai_pav);
+        laikas_pabaiga = high_resolution_clock::now();
+        auto laikas_issaugojimo_galvociai = duration_cast<milliseconds>(laikas_pabaiga - laikas_pradzia);
+
+        bendras_laika_issaugojimo_galvociai += static_cast<double>(laikas_issaugojimo_galvociai.count());
+
+        // issaugojam nuskriaustuku duomenis
+        laikas_pradzia = high_resolution_clock::now();
+        string nuskriaustukai_pav = "nuskriaustukai" + to_string(n_eil) + ".txt";
+        issaugojam_duomenis_c(nuskriaustukai, nuskriaustukai_pav);
+        laikas_pabaiga = high_resolution_clock::now();
+        auto laikas_issaugojimo_nuskriaustukai = duration_cast<milliseconds>(laikas_pabaiga - laikas_pradzia);
+
+        bendras_laika_issaugojimo_nuskriaustukai += static_cast<double>(laikas_issaugojimo_nuskriaustukai.count());
+    }
+
+    cout << "Skaiciuojamas failo " << filename << " laikas" << endl;
+
+    double vid_bendras_laikas_ivedimo = bendras_laikas_ivedimo / static_cast<double>(n);
+    double vid_bendras_laikas_rusiavimo = bendras_laikas_rusiavimo / static_cast<double>(n);
+    double vid_bendras_laikas_rusiavimo_dvi = bendras_laikas_rusiavimo_dvi / static_cast<double>(n);
+    double vid_bendras_laikas_rusiavimo_dvi_2strategija = bendras_laikas_rusiavimo_dvi_2strategija / static_cast<double>(n);
+    double vid_bendras_laikas_rusiavimo_dvi_3strategija = bendras_laikas_rusiavimo_dvi_3strategija / static_cast<double>(n);
+    double vid_bendras_laikas_rusiavimo_dvi_4strategija = bendras_laikas_rusiavimo_dvi_4strategija / static_cast<double>(n);
+    double vid_bendras_laika_issaugojimo_galvociai = bendras_laika_issaugojimo_galvociai / static_cast<double>(n);
+    double vid_bendras_laika_issaugojimo_nuskriaustukai = bendras_laika_issaugojimo_nuskriaustukai / static_cast<double>(n);
+
+    cout << "Vidutinis failo nuskaitymo laikas: " << vid_bendras_laikas_ivedimo << " milliseconds (" << vid_bendras_laikas_ivedimo / 1000.0 << " seconds)" << endl;
+    cout << "Vidutinis duomenu rusiavimo laikas: " << vid_bendras_laikas_rusiavimo << " milliseconds (" << vid_bendras_laikas_rusiavimo / 1000.0 << " seconds)" << endl;
+    cout << "Vidutinis duomenu padalijimas i dvi grupes laikas (1 strategija): " << vid_bendras_laikas_rusiavimo_dvi << " milliseconds (" << vid_bendras_laikas_rusiavimo_dvi / 1000.0 << " seconds)" << endl;
+    cout << "Vidutinis duomenu padalijimas i dvi grupes laikas (2 strategija): " << vid_bendras_laikas_rusiavimo_dvi_2strategija << " milliseconds (" << vid_bendras_laikas_rusiavimo_dvi_2strategija / 1000.0 << " seconds)" << endl;
+    cout << "Vidutinis duomenu padalijimas i dvi grupes laikas (3 strategija): " << vid_bendras_laikas_rusiavimo_dvi_3strategija << " milliseconds (" << vid_bendras_laikas_rusiavimo_dvi_3strategija / 1000.0 << " seconds)" << endl;
+    cout << "Vidutinis duomenu padalijimas i dvi grupes laikas (4 strategija): " << vid_bendras_laikas_rusiavimo_dvi_4strategija << " milliseconds (" << vid_bendras_laikas_rusiavimo_dvi_4strategija / 1000.0 << " seconds)" << endl;
+    cout << "Vidutinis galvociu irasymo i faila laikas: " << vid_bendras_laika_issaugojimo_galvociai << " milliseconds (" << vid_bendras_laika_issaugojimo_galvociai / 1000.0 << " seconds)" << endl;
+    cout << "Vidutinis nuskriaustuku irasymo i faila laikas: " << vid_bendras_laika_issaugojimo_nuskriaustukai << " milliseconds (" << vid_bendras_laika_issaugojimo_nuskriaustukai / 1000.0 << " seconds)" << endl;
+    cout << "Vidutinis bendras laikas: " << vid_bendras_laikas_ivedimo + vid_bendras_laikas_rusiavimo + vid_bendras_laika_issaugojimo_galvociai + vid_bendras_laika_issaugojimo_nuskriaustukai;
+    cout << " milliseconds (" << (vid_bendras_laikas_ivedimo + vid_bendras_laikas_rusiavimo + vid_bendras_laika_issaugojimo_galvociai + vid_bendras_laika_issaugojimo_nuskriaustukai) / 1000.0 << " seconds)" << endl;
+    cout << endl;
+}
+
 
 void rusiuojame_i_dvi_grupes(vector<studentas>& grupe, vector<studentas>& nuskriaustukai, vector<studentas>& galvociai) {
     for (const auto& student : grupe) {
@@ -236,9 +373,55 @@ void rusiuojame_i_dvi_grupes_3(vector<studentas>& grupe, vector<studentas>& nusk
     grupe.erase(partition_point, grupe.end());
 }
 
-void rusiuojame_i_dvi_grupes_4(std::vector<studentas>& grupe, std::vector<studentas>& nuskriaustukai, std::vector<studentas>& galvociai) {
+void rusiuojame_i_dvi_grupes_4(vector<studentas>& grupe, vector<studentas>& nuskriaustukai, vector<studentas>& galvociai) {
     auto partition_point = std::partition(grupe.begin(), grupe.end(), [](const studentas& student) {
         return student.rez >= 5.0;
+    });
+    // elementai tarp [students.begin(), partition_point) turi rez mazesni uz 5.0
+    // elementai tarp [partition_point, students.end()) turi rez didesni arba lygu 5.0
+    std::move(grupe.begin(), partition_point, std::back_inserter(nuskriaustukai));
+    std::move(partition_point, grupe.end(), std::back_inserter(galvociai));
+}
+
+void rusiuojame_i_dvi_grupes_c(vector<Studentas>& grupe, vector<Studentas>& nuskriaustukai, vector<Studentas>& galvociai) {
+    for (const auto& student : grupe) {
+        if (student.getRez() >= 5.0) {
+            galvociai.push_back(student);
+        } else {
+            nuskriaustukai.push_back(student);
+        }
+    }
+}
+
+void rusiuojame_i_dvi_grupes_2_c(vector<Studentas>& grupe, vector<Studentas>& nuskriaustukai) {
+    nuskriaustukai.clear();
+
+    auto iteratorius = grupe.begin();
+    // randam pirmaji studenta su rez < 5
+    while (iteratorius != grupe.end() && iteratorius->getRez() >= 5.0) {
+        ++iteratorius;
+    }
+
+    // Kai randa pirmaji studenta su rez < 5, kopijuoja visus studentus nuo jo iki galo i nuskriaustukus
+    if (iteratorius != grupe.end()) {
+        nuskriaustukai.assign(iteratorius, grupe.end());
+        grupe.erase(iteratorius, grupe.end());
+    }
+}
+
+void rusiuojame_i_dvi_grupes_3_c(vector<Studentas>& grupe, vector<Studentas>& nuskriaustukai) {
+    auto partition_point = std::partition(grupe.begin(), grupe.end(),
+        [](const Studentas& s) {
+            return s.getRez() >= 5;
+        });
+
+    nuskriaustukai.insert(nuskriaustukai.end(), std::make_move_iterator(partition_point), std::make_move_iterator(grupe.end()));
+    grupe.erase(partition_point, grupe.end());
+}
+
+void rusiuojame_i_dvi_grupes_4_c(vector<Studentas>& grupe, vector<Studentas>& nuskriaustukai, vector<Studentas>& galvociai) {
+    auto partition_point = std::partition(grupe.begin(), grupe.end(), [](const Studentas& student) {
+        return student.getRez() >= 5.0;
     });
     // elementai tarp [students.begin(), partition_point) turi rez mazesni uz 5.0
     // elementai tarp [partition_point, students.end()) turi rez didesni arba lygu 5.0
@@ -251,7 +434,6 @@ bool palyginimas_pagal_rezultata(const studentas& a, const studentas& b) {
     return a.rez > b.rez;
 }
 
-
 bool palyginimas_pagal_varda(const studentas &a, const studentas &b)
 {
     return a.vardas < b.vardas;
@@ -260,6 +442,20 @@ bool palyginimas_pagal_varda(const studentas &a, const studentas &b)
 bool palyginimas_pagal_pavarde(const studentas &a, const studentas &b)
 {
     return a.pavarde < b.pavarde;
+}
+
+bool palyginimas_pagal_rezultata_c(const Studentas& a, const Studentas& b) {
+    return a.getRez() > b.getRez();
+}
+
+bool palyginimas_pagal_varda_c(const Studentas &a, const Studentas &b)
+{
+    return a.getVardas() < b.getVardas();
+}
+
+bool palyginimas_pagal_pavarde_c(const Studentas &a, const Studentas &b)
+{
+    return a.getPavarde() < b.getPavarde();
 }
 
 
--- ivestis.cpp
+++ ivestis.cpp
@@ -1,6 +1,61 @@
 #include "headers/my_lib.h"
 #include "headers/deklaravimai.h"
 
+void ivedimas_is_file_c(vector<Studentas> &grupe, char vm_pasirinkimas, const string &duom_vard)
+{
+    ifstream duomenys(duom_vard);
+
+    if (!duomenys.is_open())
+    {
+        cout << "Neatsidare duomenys " << endl;
+        return;
+    }
+
+    string pirma_eilute;
+    getline(duomenys, pirma_eilute);
+
+    string eilute;
+    while (getline(duomenys, eilute))
+    { // kol nera pasiekta failo pabaiga skaitome po eilute
+        Studentas laikinas;
+
+        if (eilute.empty() || all_of(eilute.begin(), eilute.end(), ::isspace))
+        {
+            continue;
+        }
+
+        istringstream iss(eilute); // pasiverciame eilute i srauta is kurio galima skaityti lengviau
+
+        string v, p;
+        iss >> v >> p;
+        laikinas.setVardas(v);
+        laikinas.setPavarde(p);
+
+        int pazymiai;
+
+        while (iss >> pazymiai)
+        { // kol yra skaiciu tol skaitome
+            laikinas.setPazs(pazymiai);
+        }
+
+        if (!laikinas.getPaz().empty())
+        {
+            laikinas.setEgz(laikinas.getPaz().back());
+            laikinas.getPaz().pop_back();
+        }
+        else
+        {
+            cout << "Error: Vector is empty." << endl;
+        }
+        // skaiciuojamas galutinis balas
+        laikinas.calculateRez(vm_pasirinkimas);
+
+        grupe.push_back(laikinas);
+    }
+
+    duomenys.close();
+}
+
 void ivedimas_is_file(vector<studentas> &grupe, char vm_pasirinkimas, const string &duom_vard)
 {
     ifstream duomenys(duom_vard);
@@ -13,7 +68,7 @@ void ivedimas_is_file(vector<studentas> &grupe, char vm_pasirinkimas, const stri
 
     string pirma_eilute;
     getline(duomenys, pirma_eilute);
-    //cin.ignore();
+    // cin.ignore();
 
     string eilute;
     while (getline(duomenys, eilute))
@@ -26,11 +81,10 @@ void ivedimas_is_file(vector<studentas> &grupe, char vm_pasirinkimas, const stri
         }
 
         istringstream iss(eilute); // pasiverciame eilute i srauta is kurio galima skaityti lengviau
-        //
         iss >> laikinas.vardas >> laikinas.pavarde;
 
         int pazymiai;
-        // Read scores
+
         while (iss >> pazymiai)
         { // kol yra skaiciu tol skaitome
             laikinas.paz.push_back(pazymiai);
@@ -46,7 +100,6 @@ void ivedimas_is_file(vector<studentas> &grupe, char vm_pasirinkimas, const stri
 
     duomenys.close();
 }
-
 
 studentas atsitiktinis_ivedimas()
 {
@@ -74,6 +127,38 @@ studentas atsitiktinis_ivedimas()
     return laikinas;
 }
 
+Studentas atsitiktinis_ivedimas_c()
+{
+    Studentas laikinas;
+    cout << "Iveskite varda ir pavarde:";
+    string vardas, pavarde;
+    cin >> vardas >> pavarde;
+    laikinas.setVardas(vardas);
+    laikinas.setPavarde(pavarde);
+
+    cout << "Kiek pazymiu norite sugeneruoti? ";
+    int kiek_pazymiu;
+    cin >> kiek_pazymiu;
+
+    // Sugeneruojami pazymiai
+    //laikinas.setPaz(random_skaicius(kiek_pazymiu));
+    for (auto &a : random_skaicius(kiek_pazymiu))
+    {
+        laikinas.setPazs(a);
+    }
+    laikinas.setEgz(random_skaicius(1)[0]);
+
+    cout << "Sugeneruoti pazymiai: ";
+    for (auto &a : laikinas.getPaz())
+    {
+        cout << a << " ";
+    }
+    cout << endl;
+
+    cout << "Sugeneruotas egzamino rezultatas: " << laikinas.getEgz() << endl;
+
+    return laikinas;
+}
 
 studentas rankinis_ivedimas()
 {
@@ -154,6 +239,93 @@ studentas rankinis_ivedimas()
     cout << endl;
 
     cout << "Ivedei egzamina: " << laikinas.egz << endl;
+    cin.ignore();
+
+    return laikinas;
+}
+
+Studentas rankinis_ivedimas_c()
+{
+    Studentas laikinas;
+
+    cout << "Iveskite varda ir pavarde:";
+    string vardas, pavarde;
+    cin >> vardas >> pavarde;
+    laikinas.setVardas(vardas);
+    laikinas.setPavarde(pavarde);
+
+    cout << "Iveskite pazymius viena po kito ir paspauskite Enter, kai baigsite. Arba iveskite 'baigti' ir baigsite pazymiu ivedima." << endl;
+    string paz_tikrinimas;
+    cin.ignore();
+    while (true)
+    {
+        getline(cin, paz_tikrinimas);
+
+        if (paz_tikrinimas == "baigti" || paz_tikrinimas.empty())
+        {
+            break;
+        }
+
+        try
+        {
+            int input = std::stoi(paz_tikrinimas);
+
+            if (input < 0 || input > 10)
+            {
+                cout << "Neteisinga ivestis. Iveskite skaicius nuo 0 iki 10." << endl;
+                continue;
+            }
+            laikinas.setPazs(input);
+        }
+        catch (const std::invalid_argument &)
+        {
+            cout << "Neteisinga ivestis. Iveskite naturalu skaiciu tarp 1 ir 10: " << endl;
+        }
+        catch (const std::out_of_range &)
+        {
+            cout << "Ivestas skaicius per didelis. Iveskite mazesni skaiciu." << endl;
+        }
+    }
+
+    // Ivedamas egzamino rezultatas
+    while (true)
+    {
+        cout << "Iveskite egzamino rezultata: ";
+        string egzrez_laikinas;
+        cin >> egzrez_laikinas;
+
+        try
+        {
+            int input = std::stoi(egzrez_laikinas);
+
+            if (input < 1 || input > 10)
+            {
+                cout << "Egzamino rezultatas turi buti tarp 1 ir 10: " << endl;
+            }
+            else
+            {
+                laikinas.setEgz(input);
+                break;
+            }
+        }
+        catch (const std::invalid_argument &)
+        {
+            // Jei ivestis yra ne skaicius, prasoma ivesti is naujo
+            cout << "Neteisinga ivestis. Iveskite naturalu skaiciu tarp 1 ir 10: " << endl;
+        }
+        catch (const std::out_of_range &)
+        {
+            cout << "Ivestas skaicius per didelis. Iveskite mazesni skaiciu." << endl;
+        }
+    }
+
+    // Spausdinami ivesti pazymiai
+    cout << "Ivedei pazymius: ";
+    for (auto &a : laikinas.getPaz())
+        cout << a << " ";
+    cout << endl;
+
+    cout << "Ivedei egzamina: " << laikinas.getEgz() << endl;
     cin.ignore();
 
     return laikinas;
